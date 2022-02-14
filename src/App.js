f92319b2d2bbef3b158fce30fece95c43a09cec3
Maulana Malik Y <67380302+maulanamaleek@users.noreply.github.com>
2022-02-14 20:46:42 +0700

    pokemon context

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PokemonList from './pages/PokemonList/PokemonList';
 import PokemonDetail from './pages/PokemonDetail/PokemonDetail';
+import Navbar from './components/Navbar/Navbar';
+import Collection from './pages/Collection/Collection';
+import { PokemonProvider } from './utils/PokemonContext';
 
 const App = () => (
   <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path="detail/:id" element={<PokemonDetail />} />
-        <Route path="/" element={<PokemonList />} />
-      </Routes>
-    </BrowserRouter>
+    <PokemonProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="detail/:id" element={<PokemonDetail />} />
+          <Route path="collection" element={<Collection />} />
+          <Route path="/" element={<PokemonList />} />
+        </Routes>
+      </BrowserRouter>
+    </PokemonProvider>
   </div>
 );
 
